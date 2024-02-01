05f5a4985d28d61ddebf062d8afdb0c5bb0b7673
Tombosoa <hei.nantenaina.4@gmail.com>
2024-02-01 18:08:37 +0300

    feat: initialize project

---
+++ src/App/App.jsx
@@ -0,0 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import NotFound from '../Pages/NotFound.jsx';
+import LoginPage from '../Pages/LoginPage.jsx';
+import Home from '../Pages/Home.jsx';
+
+function App() {
+  return (
+<Router>
+  <Routes>
+  <Route exact path='/login' element={<LoginPage></LoginPage>}/>
+    <Route exact path='/' element={<Home></Home>}/>
+    <Route exact path='/*' element={<NotFound></NotFound>}/>
+  </Routes>
+</Router>
+  );
+}
+
+export default App;
