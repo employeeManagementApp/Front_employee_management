6d8f9d4c5e3215693a2da94068e40286aad43476
tokiniaina <hei.tokiniaina@gmail.com>
2024-02-02 23:49:50 +0300

    feat:Create navigation bar

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css';
+function Header () {
+    return(
+        <header>
+            <div className="NavigationBar">
+                <div className="Logo">
+                    <div>X</div>
+                    <h3>UPLABS</h3>
+                </div>
+                 <nav className="Navigation">
+                   <a href="">X</a>
+                   <a href="">UPLOAD</a>
+                   <a href="">LOGIN</a>
+                 </nav>
+
+            </div>
+        </header>
+    )  
+ }
+ export default Header
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import '../Components/Header/Header.jsx'
+import Header from "../Components/Header/Header.jsx";
+import legend from "../Components/image/legend-min.jpg";
+import './Home.css';
 
 const Home = ()=>{
     return(
-        <div>
-            Home Page
-        </div>
+      <div>
+        <Header></Header>
+       <main>
+        <img src={legend} alt="" class="image"/>
+       </main>
+      
+      </div> 
     )
 }
 
