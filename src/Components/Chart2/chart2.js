19ca6d333ee69bd45f435f8e9088f4a492465457
tokiniaina <hei.tokiniaina@gmail.com>
2024-02-07 00:29:29 +0300

    Feat:Add employe's profil

---
+++ src/Components/Chart2/chart2.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Radar } from 'react-chartjs-2';
+import './chart2.css';
+import 'chart.js/auto';
+
+
+
+const data = {
+    labels: [
+      'Curiosity',
+      'Patience',
+      'Creativity',
+      'Perseverence',
+      'Maturity',
+      'Discipline',
+      
+    ],
+    datasets: [ {
+      label: 'Personnality',
+      data: [40,40,40,40,30,40],
+      fill: false,
+      backgroundColor: 'rgb(54, 162, 235)',
+      borderColor: 'rgb(54, 162, 235)',
+      pointBackgroundColor: 'rgb(54, 162, 235)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(54, 162, 235)'
+    }]
+  };
+  const RadarChart = () => (
+    <div class="radar">
+        <Radar data={data} />
+    </div>
+);
+
+export default RadarChart;
+
+  
