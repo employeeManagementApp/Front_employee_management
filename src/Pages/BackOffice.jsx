d39b4204e255ee5205ce89b444115b19926d0a59
Tombosoa <hei.nantenaina.4@gmail.com>
2024-02-08 08:47:00 +0300

    feat: create backoffice page

---
+++ src/Pages/BackOffice.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import BackOfficeComponent from "../Components/BackOffice/BackOfficeComponent";
+
+const BackOffice =()=>{
+    return(
+        <div>
+            <BackOfficeComponent></BackOfficeComponent>
+        </div>
+    )
+} 
+export default BackOffice
