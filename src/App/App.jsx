43ff3b7d0fc07f3457e70f8e3d93f2b51163c6a7
Tombosoa <hei.nantenaina.4@gmail.com>
2024-02-08 08:51:00 +0300

    feat: add a changes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,6 +7,9 @@ import Home from '../Pages/Home.jsx';
 import Page from '../Pages/Page.jsx';
 import Profil from '../Components/Profil/Profil.jsx';
 import PrivateRoute from '../Components/PrivateRoute.jsx';
+import AuthentRoute from '../Components/AuthentRoute.jsx';
+import BackOffice from '../Pages/BackOffice.jsx';
+
 
 
 function App() {
@@ -16,7 +19,8 @@ function App() {
   <Route exact path='/' element={<LoginPage></LoginPage>}/>
   <Route path='/home' element={<PrivateRoute Component={Home}></PrivateRoute>} />
     <Route exact path='/*' element={<NotFound></NotFound>}/>
-    <Route path='/page' element={<PrivateRoute Component={Page}></PrivateRoute>} />
+    <Route path='/page' element={<AuthentRoute Component={Page} role="Employe" ></AuthentRoute>} />
+    <Route path='/backoffice' element={<AuthentRoute Component={BackOffice}  role="Admin"></AuthentRoute>} />
   </Routes>
 </Router>
   );
